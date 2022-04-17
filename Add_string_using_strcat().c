f7dfb81f97f6cb84dd9dd27abd7c31bb12d26836
Zubayer Bin Ahamed <103141454+zubayer1717@users.noreply.github.com>
2022-04-17 19:08:44 +0600

    Add files via upload

---
+++ Add_string_using_strcat().c
@@ -0,0 +1,19 @@
+/*#include<stdio.h>
+int main ()
+{
+    char a1[]="My name is ";
+    char a2[]="niloy";
+    strcat(a1,a2);
+    printf("a1 is : %s\n", a1);
+    return 0;
+}*/
+
+
+#include<stdio.h>
+int main ()
+{
+    char a1[]="my name is ";
+    strcat(a1,"niloy");
+    printf("a1 = %s\n",a1);
+    return 0;
+}
